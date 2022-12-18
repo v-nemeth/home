440bc69b3a7e6329d3e098388e87d1cb43ac9db4
v-nemeth <viand21@student.sdu.dk>
2022-12-18 13:12:11 +0100

    edited game_sessions to have a game_id

--- db/migrate/20221218120249_create_game_sessions.rb
+++ db/migrate/20221218120249_create_game_sessions.rb
@@ -1,7 +1,7 @@
 class CreateGameSessions < ActiveRecord::Migration[7.0]
   def change
     create_table :game_sessions do |t|
-      t.string :game
+      t.integer :game_id
       t.integer :winner_id
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_10_164709) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_18_120454) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "game_sessions", force: :cascade do |t|
+    t.integer "game_id"
+    t.integer "winner_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "games", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "pages", force: :cascade do |t|
     t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "players", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
